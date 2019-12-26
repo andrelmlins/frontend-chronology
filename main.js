81fc044bd0489cae849c4b5e39a5431d9292bb38
André Lins <andre.lins@softplan.com.br>
2019-12-26 18:47:48 -0300

    Effect in show libraries

--- main.js
+++ main.js
@@ -13,27 +13,61 @@ fetch("./list.json")
       container.setAttribute("class", `container ${side}`);
 
       container.innerHTML = `
-        <div class="content" role="section">
-          <div class="content-logo">
+        <div class="content content-${side} js--fadeIn${side}" role="section">
+          <div class="content-logo content-logo-${side}">
             <img
               class="logo"
               alt="${item.name}"
               src="${item.imageUrl}"
             />
-            </div>
-            <div class="content-body">
-              <h3>${item.year} - <strong>${item.name}</strong></h3>
-              <p>${item.description}</p>
-              <a aria-label="Website" class="link" href="${item.website}" target="_blank">
-                <i class="fa fa-link"></i>
-              </a>
-              <a aria-label="Github" class="link" href="${item.github}" target="_blank">
-                <i class="fa fa-github"></i>
-              </a>
-            </div>
+          </div>
+          <div class="content-body">
+            <h3>${item.year} - <strong>${item.name}</strong></h3>
+            <p>${item.description}</p>
+            <a aria-label="Website" class="link" href="${item.website}" target="_blank">
+              <i class="fa fa-link"></i>
+            </a>
+            <a aria-label="Github" class="link" href="${item.github}" target="_blank">
+              <i class="fa fa-github"></i>
+            </a>
+          </div>
         </div>
       `;
 
       timeline.appendChild(container);
     });
   });
+
+window.onload = () => {
+  window.sr = ScrollReveal();
+
+  if (window.innerWidth < 768) {
+    sr.reveal(".js--fadeInleft", {
+      origin: "right",
+      distance: "100px",
+      easing: "ease-in-out",
+      duration: 800
+    });
+
+    sr.reveal(".js--fadeInright", {
+      origin: "right",
+      distance: "100px",
+      easing: "ease-in-out",
+      duration: 800
+    });
+  } else {
+    sr.reveal(".js--fadeInleft", {
+      origin: "left",
+      distance: "200px",
+      easing: "ease-in-out",
+      duration: 800
+    });
+
+    sr.reveal(".js--fadeInright", {
+      origin: "right",
+      distance: "300px",
+      easing: "ease-in-out",
+      duration: 800
+    });
+  }
+};
