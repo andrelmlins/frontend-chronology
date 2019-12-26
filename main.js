75af02c66457bdd9531f390fb2a493f0722fcf4a
André Lins <andre.lins@softplan.com.br>
2019-12-26 14:55:31 -0300

    Inserting corner and a11y

--- main.js
+++ main.js
@@ -13,20 +13,21 @@ fetch("./list.json")
       container.setAttribute("class", `container ${side}`);
 
       container.innerHTML = `
-        <div class="content">
+        <div class="content" role="section">
           <div class="content-logo">
             <img
               class="logo"
-              src=${item.imageUrl}
+              alt="${item.name}"
+              src="${item.imageUrl}"
             />
             </div>
             <div class="content-body">
               <h3>${item.year} - <strong>${item.name}</strong></h3>
               <p>${item.description}</p>
-              <a class="link" href="${item.website}" target="_blank">
+              <a aria-label="Website" class="link" href="${item.website}" target="_blank">
                 <i class="fa fa-link"></i>
               </a>
-              <a class="link" href="${item.github}" target="_blank">
+              <a aria-label="Github" class="link" href="${item.github}" target="_blank">
                 <i class="fa fa-github"></i>
               </a>
             </div>
