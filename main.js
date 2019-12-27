c3e1428f8ebda4e38ee7b1cf9d242a6983531a09
André Lins <andre.lins@softplan.com.br>
2019-12-27 12:38:09 -0300

    Adjustment in ScrollReveal

--- main.js
+++ main.js
@@ -8,40 +8,40 @@ const showModal = name => {
     .then(() => {
       const nameGithub = project.github.replace("https://github.com/", "");
 
-      document.body.innerHTML += `
-        <div id="modalProject" class="modal" role="alertdialog">
-          <div class="modal-content" role="document">
-            <span class="close" onclick="closeModal()">&times;</span>
-            <h2 style="margin: 0">${project.name}</h2>
-            <div style="margin-top: 20px">
-              <img alt="GitHub stars" src="https://img.shields.io/github/stars/${nameGithub}?style=social">
-              <img alt="GitHub watchers" src="https://img.shields.io/github/watchers/${nameGithub}?style=social">
-              <img alt="GitHub forks" src="https://img.shields.io/github/forks/${nameGithub}?style=social">
-            </div>
-            <div style="display:flex; margin: 20px 0px">
-              <div class="modal-logo">
-                <img
-                  class="logo"
-                  alt="${project.name}"
-                  src="${project.imageUrl}"
-                />
-              </div>
-              <p style="margin:0">${project.description}</p>
-            </div>
-            <a aria-label="Website" class="link" href="${project.website}" target="_blank">
-              <i class="fa fa-link"></i>
-            </a>
-            <a aria-label="Github" class="link" href="${project.github}" target="_blank">
-              <i class="fa fa-github"></i>
-            </a>
+      document.getElementById("modalProject").innerHTML += `
+        <div class="modal-content" role="document">
+          <span class="close" onclick="closeModal()">&times;</span>
+          <h2 style="margin: 0">${project.name}</h2>
+          <div style="margin-top: 20px">
+            <img alt="GitHub stars" src="https://img.shields.io/github/stars/${nameGithub}?style=social">
+            <img alt="GitHub watchers" src="https://img.shields.io/github/watchers/${nameGithub}?style=social">
+            <img alt="GitHub forks" src="https://img.shields.io/github/forks/${nameGithub}?style=social">
           </div>
+          <div style="display:flex; margin: 20px 0px">
+            <div class="modal-logo">
+              <img
+                class="logo"
+                alt="${project.name}"
+                src="${project.imageUrl}"
+              />
+            </div>
+            <p style="margin:0">${project.description}</p>
+          </div>
+          <a aria-label="Website" class="link" href="${project.website}" target="_blank">
+            <i class="fa fa-link"></i>
+          </a>
+          <a aria-label="Github" class="link" href="${project.github}" target="_blank">
+            <i class="fa fa-github"></i>
+          </a>
         </div>
       `;
+      document.getElementById("modalProject").style.display = "flex";
     });
 };
 
 const closeModal = () => {
-  document.body.removeChild(document.getElementById("modalProject"));
+  document.getElementById("modalProject").style.display = "none";
+  document.getElementById("modalProject").innerHTML = "";
 };
 
 window.onload = () => {
@@ -95,32 +95,28 @@ window.onload = () => {
           origin: "right",
           distance: "100px",
           easing: "ease-in-out",
-          duration: 800,
-          container: ".timeline"
+          duration: 800
         });
 
         sr.reveal(".js--fadeInright", {
           origin: "right",
           distance: "100px",
           easing: "ease-in-out",
-          duration: 800,
-          container: ".timeline"
+          duration: 800
         });
       } else {
         sr.reveal(".js--fadeInleft", {
           origin: "left",
           distance: "200px",
           easing: "ease-in-out",
-          duration: 800,
-          container: ".timeline"
+          duration: 800
         });
 
         sr.reveal(".js--fadeInright", {
           origin: "right",
           distance: "300px",
           easing: "ease-in-out",
-          duration: 800,
-          container: ".timeline"
+          duration: 800
         });
       }
     });
